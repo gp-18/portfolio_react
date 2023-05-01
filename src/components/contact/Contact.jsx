a18af37e66387b327c049e0ff268c1bbea2bdf70
alwaysalazycoder <gs314315316@gmail.com>
2023-05-02 00:15:17 +0530

    Input tag issue

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -26,14 +26,16 @@ const Contact = () => {
         </article>
         </div>
         <form action="">
-          <input type="text" name ="name" placeholder='Your Full Name' required> </input>
-          <input type="email" name ="email" placeholder='Your Email' required></input>
-          <textarea name='message' rows={7} placeholder='Your Message' required> </textarea>
+          <input type="text" name ="name" placeholder='Your Full Name' required/> 
+          <input type="email" name ="email" placeholder='Your Email' required/>
+          <textarea name='message' rows={7} placeholder='Your Message' required/> 
           <button type="submit" className='btn btn-primary'>Send Message</button>
         </form>
       </div>
 
     </section>
+
+  
   )
 }
 
